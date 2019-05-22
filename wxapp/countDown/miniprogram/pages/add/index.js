74de5acdbf494ca5575c3dc723d721d89186fd1d
chenrui668 <1941718132@qq.com>
2019-05-22 17:45:12 +0800

    add files

--- wxapp/countDown/miniprogram/pages/add/index.js
+++ wxapp/countDown/miniprogram/pages/add/index.js
@@ -5,12 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    inputValue: '',
+    num: 0,
+    height: 0,
+    width: 0,
+    showTitle: false  
   },
-  onLoad: function (options) {
-
+  onLoad: function () {
+    this.setData({
+      height: wx.getSystemInfoSync().windowHeight,
+      width: wx.getSystemInfoSync().windowWidth
+    })
   },
   back () {
     wx.navigateBack();
+  },
+  bindKeyInput(e) {
+    this.setData({
+      inputValue: e.detail.value,
+      num: e.detail.value.length
+    })
+  },
+  chooseTitle () {
+    this.setData({
+      showTitle: true
+    })
+  },
+  hideTitle () {
+    this.setData({
+      showTitle: false
+    })
   }
 })
