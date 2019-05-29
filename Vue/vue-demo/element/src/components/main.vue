b9fe826ad1f87207482c7f5d58d8ca409ac1eef2
chenrui668 <1941718132@qq.com>
2019-05-29 22:21:01 +0800

    add files

---
+++ Vue/vue-demo/element/src/components/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-content">
+    <Header></Header>
+    <Slider></Slider>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/Header'
+import Slider from '@/components/common/Slide'
+export default {
+  name: 'mainContent',
+  data () {
+    return {
+      msg: 'hello world'
+    }
+  },
+  components: {
+    Header,
+    Slider
+  }
+}
+</script>
+
--- Vue/vue-demo/element/src/router/index.js
+++ Vue/vue-demo/element/src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
-
+import Main from '@/components/Main'
+import MainDetail from '@/components/MainDetail'
+import UserList from '@/components/UserList'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +18,23 @@ export default new Router({
       path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/main',
+      name: Main,
+      component: Main,
+      children: [
+        {
+          path: '/',
+          name: MainDetail,
+          component: MainDetail
+        },
+        {
+          path: '/main/user-list',
+          name: UserList,
+          component: UserList
+        }
+      ]
     }
   ]
 })
