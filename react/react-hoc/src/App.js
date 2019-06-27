1fc103b4b1f4c03f4123103ecff01439b5d93b74
chenrui668 <1941718132@qq.com>
2019-06-27 11:57:19 +0800

    add files

--- react/react-hoc/src/App.js
+++ react/react-hoc/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import LoginStatus from './LoginStatus';
+import ExampleMobx from './exampleMobx/index';
 import ShopCart from './ShopCart';
 import WithLogin from './WithLogin';
+import examplebase from './examplebase/index';
 import './App.css';
 
 // const WithLoginStatus = WithLogin(LoginStatus);
@@ -14,6 +16,7 @@ function App() {
       <ShopCart a="1" b="2"/>
       {/* <WithLoginStatus />
       <WithShopCart a="1" b="2"/> */}
+      <ExampleMobx></ExampleMobx>
     </React.Fragment>
   );
 }
