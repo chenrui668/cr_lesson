cf53a13c03332db9b7fa546a41788a4fd7c05483
chenrui668 <1941718132@qq.com>
2019-07-02 14:27:43 +0800

    add files

---
+++ react/music-app/src/components/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect, NavLink} from 'react-router-dom';
+import Recommend from './recommend/Recommend';
+import Ranking from './ranking/Ranking';
+import Search from './search/Search';
+import './App.styl';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <header className="app-header">
+          header
+        </header>
+        <div className="music-tab">
+          <div className="tab-item">
+            <NavLink className="nav-link" to="/recommend">
+              <span>推荐</span>
+            </NavLink>
+          </div>
+          <div className="tab-item">
+            <NavLink className="nav-link" to="/ranking">
+              <span>排行榜</span>
+            </NavLink>
+          </div>
+          <div className="tab-item">
+            <NavLink className="nav-link" to="/search">
+              <span>搜索</span>
+            </NavLink>
+          </div>
+        </div>
+        <div className="music-view">
+          {/* 路由 */}
+          <Route path="/recommend" component={Recommend}></Route>
+          <Route path="/ranking" component={Ranking}></Route>
+          <Route path="/search" component={Search}></Route>
+          <Redirect from="/" to="/recommend"></Redirect>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ react/music-app/src/components/ranking/Ranking.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Ranking extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                ranking
+            </div>
+        );
+    }
+}
+ 
+export default Ranking;
