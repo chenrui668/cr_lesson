7a50812d9fb808abd4ff294b1ccf3dd9c643d6af
chenrui668 <1941718132@qq.com>
2019-07-10 17:14:03 +0800

    add files

---
+++ Vue/component-a/src/components/com/display.vue
@@ -0,0 +1,20 @@
+<template>
+    <div ref="display">
+        123
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        code: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
